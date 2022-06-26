be8163d4b0adb22f877b8e2f88fc14dc494ee453
carolinevsboliveira <carolinevsboliveira@gmail.com>
2022-06-26 16:05:29 -0400

    fix: use swc config at jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
-module.exports = {
+/* eslint-disable-next-line*/
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({ dir: './' })
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', './next'],
   collectCoverage: false,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  collectCoverageFrom: ['src/**/*.ts(x)']
 }
+
+module.exports = createJestConfig(customJestConfig)
